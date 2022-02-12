267536cf7cfdfaed8de93314b73eddbfd98c58d9
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2022-02-12 14:39:21 +0600

    midified p2

--- problem-2.py
+++ problem-2.py
@@ -1,19 +1,13 @@
 # Qtec - Assignments
 def searchString(strings,patterns):
     """Find pattern from string and return count"""
-    # print(strings,patterns)
-    slen = len(strings)
-    plen =  len(patterns)
-    count = 0
+    slen,plen,count = len(strings),len(patterns),0     # string lenght, pattern lenght, number of matched patterns
     for i in range(slen+1):
-        print(i,strings[i:i+plen])
         if strings[i:i+plen] == patterns:
             count += 1
     return count
-
 ################################
 strings = "tadadattaetadadadafa"
 patterns = "dada"
 result = searchString(strings,patterns)
-print(f"Total Count: {result}.")
-
+print(f"Total matched pattern count: {result}.")
